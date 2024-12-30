aa3a5cf47f70163c90b28c4b2687c2f3d25bf1df
Suryaraj Timsina <timsinasuryaraj@gmail.com>
2024-12-30 16:24:43 +0545

    Initial commit

---
+++ src/main/java/com/techaxis/HelloDevOpsApp.java
@@ -0,0 +1,7 @@
+package com.techaxis;
+
+public class HelloDevOpsApp {
+    public static void main(String[] args) {
+        System.out.println("hello devops");
+    }
+}
---
+++ src/test/java/com/techaxis/HelloDevOpsAppTest.java
@@ -0,0 +1,11 @@
+package com.techaxis;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class HelloDevOpsAppTest {
+    @Test
+    public void testOutput() {
+        assertEquals("Basic test for 'hello devops'", "hello devops", "hello devops");
+    }
+}
