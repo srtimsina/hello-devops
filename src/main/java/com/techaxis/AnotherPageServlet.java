5346b80197422338069ef1d6714a3eff758c6afa
Suryaraj Timsina <timsinasuryaraj@gmail.com>
2025-01-02 16:46:09 +0545

    Add new page

---
+++ src/main/java/com/techaxis/AnotherPageServlet.java
@@ -0,0 +1,18 @@
+package com.techaxis;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/anotherPage")
+public class AnotherPageServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.getWriter().write("<h1>Another Page</h1><p>Welcome to another page!</p>");
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,14 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+    <h2>Welcome to TechAxis WebApp</h2>
+    <p>Hello, World! You can navigate to different pages below:</p>
+
+    <ul>
+        <!-- Link to the HelloServlet -->
+        <li><a href="${pageContext.request.contextPath}/hello">Go to Hello Servlet</a></li>
+
+        <!-- Link to the AnotherPageServlet -->
+        <li><a href="${pageContext.request.contextPath}/anotherPage">Go to Another Page</a></li>
+    </ul>
 </body>
 </html>
